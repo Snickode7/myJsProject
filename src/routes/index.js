87df3edb955a69d4377cfed8b7a34a49f0c7f863
Nick Matheis <nickmath21@gmail.com>
2017-07-11 21:08:32 -0400

    Body-parser

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,15 +6,28 @@ router.use('/doc', function(req, res, next) {
 });
 
 router.get('/file', function(req, res, next) {
-  res.end('List all files');
+  res.json(FILES);
 });
 
 router.post('/file', function(req, res, next) {
-  res.end('Create a new file');
+  const newId = '' + FILES.length;
+  const data = req.body;
+  data.id = newId;
+
+  FILES.push(data);
+  res.status(201).json(data);
 });
 
 router.put('/file/:fileId', function(req, res, next) {
-  res.end(`Updating file '${req.params.fileId}'`);
+  const {fileId} = req.params;
+  const file = FILES.find(entry => entry.id === fileId);
+  if (!file) {
+    return res.status(404).end(`Could not find file '${fileId}'`);
+  }
+
+  file.title = req.body.title;
+  file.description = req.body.description;
+  res.json(file);
 });
 
 router.delete('/file/:fileId', function(req, res, next) {
@@ -22,7 +35,19 @@ router.delete('/file/:fileId', function(req, res, next) {
 });
 
 router.get('/file/:fileId', function(req, res, next) {
-  res.end(`Reading file '${req.params.fileId}'`);
+  const {fileId} = req.params;
+  // same as 'const fileId = req.params.fileId'
+
+  const file = FILES.find(entry => entry.id === fileId);
+  if (!file) {
+    return res.status(404).end(`Could not find file '${fileId}'`);
+  }
+
+  res.json(file);
 });
+
+//router.get('/file/:fileId', function(req, res, next) {
+  //res.end(`Reading file '${req.params.fileId}'`);
+//});
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,8 +2,23 @@ const path = require('path');
 const express = require('express');
 const config = require('./config');
 const router = require('./routes');
+const bodyParser = require('body-parser');
 
 const trailTrackerApp = express();
+
+trailTrackerApp.use(function(req, res, next) {
+  console.log("req.body BEFORE parsing", req.body);
+  next();
+});
+
+trailTrackerApp.use(bodyParser.json());
+
+trailTrackerApp.use(function(req, res, next) {
+  console.log("req.body AFTER parsing", req.body);
+  next();
+});
+
+
 
 const publicPath = path.resolve(__dirname, '../public');
 trailTrackerApp.use(express.static(publicPath));
